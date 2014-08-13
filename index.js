7cc54bd4dc647451cfd64006bf24ac8b765232c3
Nikolaus Piccolotto <nikolaus.piccolotto@zalando.de>
2014-08-13 22:29:56 +0200

    working with tests

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 'use strict';
 
-var gutil = require( 'gulp-util' ),
-    through = require( 'through2' ),
-    css = require( 'css' ),
-    fs = require( 'fs' ),
-    glob = require( 'glob' ),
-    Q = require( 'q' ),
-    html = require( 'htmlparser2' ),
+var gutil = require( 'gulp-util' ),     // for gulp plugin error
+    through = require( 'through2' ),    // stream library
+    css = require( 'css' ),             // css parser
+    fs = require( 'fs' ),               // file system access
+    glob = require( 'glob' ),           // to read globs like src/main/webapp/**/*.html
+    Q = require( 'q' ),                 // promise implementation
+    html = require( 'htmlparser2' ),    // html parser
 
     PLUGIN_NAME = 'gulp-check-unused-css';
 
 var definedClasses = [],
     usedClasses = [],
-    CLASS_REGEX = /\.[a-zA-Z](?:[0-9A-Za-z_-])+/g;
+    CLASS_REGEX = /\.[a-zA-Z](?:[0-9A-Za-z_-])+/g;  // leading dot followed by a letter followed by digits, letters, _ or -
 
 function isClass( def ) {
     return CLASS_REGEX.test( def );
 }
 
+// checks if the selectors of a CSS rule are a class
+// an adds them to the defined classes
 function getClasses( rule ) {
     if ( !rule.type === 'rule ' ) {
         return;
@@ -29,31 +31,28 @@ function getClasses( rule ) {
         }
         matches.forEach( function( match ) {
             if ( definedClasses.indexOf( match ) === -1 ) {
-                console.log( 'found class in css', match );
                 definedClasses.push( match );
             }
         });
     });
 }
 
-function writeFileInStream( stream ) {
-    return function( e, file ) {
-        if ( e ) return;
-
-        stream.write( file );
-    };
-}
-
+// actual function that gets exported
 function checkCSS( opts ) {
+
+    // clear arrays just in case
     definedClasses.splice();
     usedClasses.splice();
 
+
+    // create html parser
     var htmlparser = new html.Parser({
         onopentag: function onopentag( name, attribs ) {
-            if ( attribs['class'] ) {
-                var used = attribs['class'].split( ' ' );
+            if ( attribs[ 'class' ] ) {
+                // if we find an open tag with class attribute, add those to used classes
+                // this will also find classes on script tags, but whatever
+                var used = attribs[ 'class' ].split( ' ' );
                 used.forEach( function( usedClass ) {
-                    console.log( 'found class in template:', usedClass );
                     if ( usedClasses.indexOf( usedClass ) === -1 ) {
                         usedClasses.push( usedClass );
                     }
@@ -63,44 +62,68 @@ function checkCSS( opts ) {
     });
 
     var files,
-        filesRead = Q.defer();
-    if ( opts.files ) {
-        glob( opts.files, null, function( err, globFiles ) {
+        filesRead = Q.defer();  // resolves when all files are read by glob
 
+    if ( opts && opts.files ) {
+
+        glob( opts.files, null, function( err, globFiles ) {
+            // put all files in html parser
             globFiles.forEach( function( filename ) {
-                var file = fs.readFileSync( filename, 'utf-8' );
+                var file = fs.readFileSync( filename, 'utf8' );
                 htmlparser.write( file );
             });
+
             filesRead.resolve();
         });
+    } else {
+        // throw an error if there are no html files configured
+        throw new gutil.PluginError( PLUGIN_NAME, 'No HTML files specified' );
+        return done();
     }
 
-    return through.obj( function( file, enc, cb ) {
+    return through.obj( function( file, enc, done ) {
+        var self = this;
+
         if ( file.isNull() ) {
-            return cb();
+            self.push( file );
+            return done();
         }
 
         if ( file.isStream()) {
-            this.emit( 'error', new gutil.PluginError( PLUGIN_NAME, 'Streaming not supported' ) );
-            return cb();
+            self.emit( 'error', new gutil.PluginError( PLUGIN_NAME, 'Streaming not supported' ) );
+            return done();
         }
 
         filesRead.promise.then( function() {
-            var ast = css.parse( file );
-            // find all classes
+
+            // parse css content
+            var ast = css.parse( String( file.contents ) ),
+                unused = [];
+
+            // find all classes in CSS
             ast.stylesheet.rules.forEach( getClasses );
-            // remove leading dot because that's not in the html
-            definedClasses = definedClasses.map( function( classdef ) {
-                return classdef.substring( 1 );
-            });
+            
+            unused = definedClasses
+                        // remove leading dot because that's not in the html
+                        .map( function( classdef ) {
+                            return classdef.substring( 1 );
+                        })
+                        // filter unused
+                        .filter( function( definedClass ) {
+                            return usedClasses.indexOf( definedClass ) === -1;
+                        });
 
-            var allUsed = definedClasses.every( function( definedClass ) {
-                return usedClasses.indexOf( definedClass ) >= 0;
-            });
+            // throw an error if there are unused classes
+            if ( unused.length > 0 ) {
+                var error = new Error( 'The following classes in your CSS are actually unused: ' + unused.join( ' ' ) );
+                error.unused = unused;
+                self.emit( 'error', new gutil.PluginError( PLUGIN_NAME, error ) );
+                return done();
+            }
 
-            console.log( allUsed );
-
-            cb();
+            // else proceed
+            self.push( file );
+            done();
         });
     });
 }
---
+++ test/test.js
@@ -0,0 +1,98 @@
+var checkCSS = require( '../index' ),
+    gutil = require( 'gulp-util' ),
+    assert = require( 'assert' ),
+    fs = require( 'fs' ),
+    sinon = require( 'sinon' ),
+    css = fs.readFileSync( 'test/test.css', 'utf8' ),
+    stream,
+    bufferedCSS;
+
+beforeEach( function() {
+    bufferedCSS = new gutil.File({
+        path: 'test/test.css',
+        contents: new Buffer( css, 'utf8' )
+    });
+});
+
+afterEach( function() {
+    bufferedCSS = null;
+});
+
+it( 'should throw an error in bad case', function( done ) {
+    var dataSpy = sinon.spy(),
+        stream = checkCSS({
+            files: 'test/b*.html'
+        });
+
+    stream.on( 'data', dataSpy );
+    stream.on( 'error', function( err ) {
+        // check correct class
+        assert.equal( err.unused.length, 1 );
+        assert.equal( err.unused[ 0 ], 'special' );
+        // check that no file was emitted
+        assert.equal( dataSpy.called, false );
+
+        done();
+    });
+
+    stream.write( bufferedCSS );
+    stream.end();
+});
+
+it( 'should emit the file in happy case', function( done ) {
+    var errorSpy = sinon.spy(),
+        stream = checkCSS({
+            files: 'test/h*.html'
+        });
+
+    stream.on( 'error', errorSpy );
+
+    stream.on( 'data', function( buffered ) {
+        // check that file is the same
+        assert.equal( String( buffered.contents ), css );
+        // check that no error was thrown
+        assert.equal( errorSpy.called, false );
+
+        done();
+    });
+
+    stream.write( bufferedCSS );
+    stream.end();
+});
+
+it( 'should throw an error if there is no config', function( done ) {
+    try {
+        stream = checkCSS();
+    } catch( ex ) {
+        assert.equal( ex.message, 'No HTML files specified' );
+        done();
+    }
+});
+
+it( 'should throw an error if there are no HTML files specified', function( done ) {
+    try {
+        stream = checkCSS({ whatever: 'yeah' });
+    } catch( ex ) {
+        assert.equal( ex.message, 'No HTML files specified' );
+        done();
+    }
+});
+
+it( 'should let null files through', function( done ) {
+    var errorSpy = sinon.spy(),
+        stream = checkCSS({
+            files: 'test/b*.html'
+        });
+
+    stream.on( 'error', errorSpy );
+    stream.on( 'data', function() {
+        assert.equal( errorSpy.called, false );
+        done();
+    });
+    stream.write( new gutil.File({
+        path: 'null',
+        contents: null
+    }));
+
+    stream.end();
+});
