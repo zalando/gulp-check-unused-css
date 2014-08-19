f8ddb5b658a466d3883f42523019cfe067eccbeb
Nikolaus Piccolotto <nikolaus.piccolotto@zalando.de>
2014-08-19 11:00:42 +0200

    Add license hint to source file

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+;
+/**
+ *  Copyright 2014 Zalando SE
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+**/
+
 'use strict';
 
 var gutil = require( 'gulp-util' ),     // for gulp plugin error
